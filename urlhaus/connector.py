1052f63435c1d4998821bf862112da6d31032c5e
IslamkamalBaker <islam_k_baker@yahoo.com>
2023-06-01 12:46:12 +0300

    Job complete
    All note has been modified as recommendation from R&D

--- urlhaus/connector.py
+++ urlhaus/connector.py
@@ -1,6 +1,6 @@
 """
 Copyright start
-Copyright (C) 2008 - 2021 Fortinet Inc.
+Copyright (C) 2008 - 2023 Fortinet Inc.
 All rights reserved.
 FORTINET CONFIDENTIAL & FORTINET PROPRIETARY SOURCE CODE
 Copyright end
